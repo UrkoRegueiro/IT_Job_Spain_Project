a67aab2adda57fb4eacf3106efb0e5c4c6764a76
Urko <127695145+UrkoRegueiro@users.noreply.github.com>
2024-02-19 15:16:28 +0100

    updated

--- Streamlit/acercade.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def info():
-
-    st.markdown("HOLA pattata")
-
-if __name__ == "__modelo__":
-    modelo()
--- Streamlit/funciones/config.py
+++
@@ -1,9 +0,0 @@
-
-PAGE_CONFIG = {"page_title"           : "Applica Tech",
-               "page_icon"            : None,
-               "layout"               : "wide",
-               "initial_sidebar_state": "auto",
-               "menu_items"           : {'Get Help': 'https://www.extremelycoolapp.com/help',
-                                         'Report a bug': "https://www.extremelycoolapp.com/bug",
-                                         'About': "# This is a header. This is an *extremely* cool app!"
-                                        }
